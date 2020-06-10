a45f123871b36cd57327032b12312e1cd04d477d
github username <a.gaysenko@gmail.com>
2020-06-09 22:50:47 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog
-    def name=(dog_name)  # defined instance method taking an argument of a dogs name 
-        @this_dogs_name = dog_name   #taking the arguement and setting it to a variable
+    def name=(dogs_name)  # defined instance method taking an argument of a dogs name 
+        @this_dogs_name = dogs_name   #taking the arguement and setting it to a variable
 end
 
 def name         #is the actual method
     @this_dogs_name
     end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
 
 
 
